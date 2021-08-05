926a9064d0d74e9cc2dba1fadfc4c6ae4f3d1ecf
Eric Rabil <ericjrabil@gmail.com>
2021-08-05 09:56:21 -0400

    Bump iOS version requirement to 9.0

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Swog",
     platforms: [
         .macOS(.v10_12),
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
